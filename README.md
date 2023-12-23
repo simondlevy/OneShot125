d067cc9a716a4149f43e56a470de255f8cd3803f
simondlevy <simon.d.levy@gmail.com>
2023-12-22 23:24:31 -0500

    =More README

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 <img src="image.jpg" width=500>
->
-https://github.com/nickrehm/dRehmFlight
+
+This a simple, header-only C++ library for using a 
+[Teensy](https://www.pjrc.com/teensy/)
+microncontroller board to run brushless motors via an electronic speed
+controller (ESC) supporting the popular
+[OneShot125](https://oscarliang.com/oneshot125-esc-quadcopter-fpv/) protocol.  I adpated the code from
+Nicholas Rhem's awesome
+[dRehmFlight](https://github.com/nickrehm/dRehmFlight) repository, with the goal of allowing
+fellow Teensy lovers to use this kind of ESC in other projects.
--- src/oneshot125.hpp
+++ src/oneshot125.hpp
@@ -1,3 +1,24 @@
+/*
+   Header-only library for running OneShot125 ESCs from a Teensy board
+
+   Adapted from the code in https://github.com/nickrehm/dRehmFlight
+
+   This file is part of Teensy-OneShot125.
+
+   Teensy-OneShot125 is free software: you can redistribute it and/or modify it
+   under the terms of the GNU General Public License as published by the Free
+   Software Foundation, either version 3 of the License, or (at your option)
+   any later version.
+
+   Teensy-OneShot125 is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+   or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+   more details.
+
+   You should have received a copy of the GNU General Public License along with
+   Teensy-OneShot125. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #include <PWMServo.h> 
 
 #include <stdint.h>
