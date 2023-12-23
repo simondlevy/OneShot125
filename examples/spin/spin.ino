a35ba4d461b80c14e34d08814b3eb3b850fd51eb
simondlevy <simon.d.levy@gmail.com>
2023-12-22 20:35:03 -0500

    =Cleanup

--- examples/spin/spin.ino
+++ examples/spin/spin.ino
@@ -15,13 +15,8 @@ static void loopRate(int freq) {
 }
 
 static int m1Pin = 0;
-static int m2Pin = 1;
-static int m3Pin = 2;
-static int m4Pin = 3;
-static int m5Pin = 4;
-static int m6Pin = 5;
 
-int m1_command_PWM, m2_command_PWM, m3_command_PWM, m4_command_PWM, m5_command_PWM, m6_command_PWM;
+int m1_command_PWM;
 
 static void commandMotors() {
 
@@ -30,52 +25,21 @@ static void commandMotors() {
   int pulseStart, timer;
 
   int flagM1 = 0;
-  int flagM2 = 0;
-  int flagM3 = 0;
-  int flagM4 = 0;
-  int flagM5 = 0;
-  int flagM6 = 0;
 
   digitalWrite(m1Pin, HIGH);
-  digitalWrite(m2Pin, HIGH);
-  digitalWrite(m3Pin, HIGH);
-  digitalWrite(m4Pin, HIGH);
-  digitalWrite(m5Pin, HIGH);
-  digitalWrite(m6Pin, HIGH);
 
   pulseStart = micros();
 
-  while (wentLow < 6 ) { 
+  while (wentLow < 1) { 
+
     timer = micros();
+
     if ((m1_command_PWM <= timer - pulseStart) && (flagM1==0)) {
       digitalWrite(m1Pin, LOW);
       wentLow = wentLow + 1;
       flagM1 = 1;
     }
-    if ((m2_command_PWM <= timer - pulseStart) && (flagM2==0)) {
-      digitalWrite(m2Pin, LOW);
-      wentLow = wentLow + 1;
-      flagM2 = 1;
-    }
-    if ((m3_command_PWM <= timer - pulseStart) && (flagM3==0)) {
-      digitalWrite(m3Pin, LOW);
-      wentLow = wentLow + 1;
-      flagM3 = 1;
-    }
-    if ((m4_command_PWM <= timer - pulseStart) && (flagM4==0)) {
-      digitalWrite(m4Pin, LOW);
-      wentLow = wentLow + 1;
-      flagM4 = 1;
-    } 
-    if ((m5_command_PWM <= timer - pulseStart) && (flagM5==0)) {
-      digitalWrite(m5Pin, LOW);
-      wentLow = wentLow + 1;
-      flagM5 = 1;
-    } 
-    if ((m6_command_PWM <= timer - pulseStart) && (flagM6==0)) {
-      digitalWrite(m6Pin, LOW);
-      wentLow = wentLow + 1;
-      flagM6 = 1;
+
     } 
   }
 }
@@ -94,22 +58,11 @@ void setup()
 
   delay(500);
 
-  pinMode(13, OUTPUT); 
   pinMode(m1Pin, OUTPUT);
-  pinMode(m2Pin, OUTPUT);
-  pinMode(m3Pin, OUTPUT);
-  pinMode(m4Pin, OUTPUT);
-  pinMode(m5Pin, OUTPUT);
-  pinMode(m6Pin, OUTPUT);
 
   delay(15);
 
   m1_command_PWM = 125; 
-  m2_command_PWM = 125;
-  m3_command_PWM = 125;
-  m4_command_PWM = 125;
-  m5_command_PWM = 125;
-  m6_command_PWM = 125;
 
   armMotors(); 
 }
