4888f51a0dfb3797b2368a0de824a42a635ed063
simondlevy <simon.d.levy@gmail.com>
2023-12-22 20:25:05 -0500

    =Cleanup

--- examples/spin/spin.ino
+++ examples/spin/spin.ino
@@ -1,5 +1,7 @@
 #include <PWMServo.h> 
 
+#include <stdint.h>
+
 static unsigned long current_time;
 
 static void loopRate(int freq) {
@@ -12,15 +14,6 @@ static void loopRate(int freq) {
   }
 }
 
-static float thro_des;
-
-static unsigned long channel_1_fs = 1000; 
-static unsigned long channel_2_fs = 1500; 
-static unsigned long channel_3_fs = 1500; 
-static unsigned long channel_4_fs = 1500; 
-static unsigned long channel_5_fs = 2000; 
-static unsigned long channel_6_fs = 2000; 
-
 static int m1Pin = 0;
 static int m2Pin = 1;
 static int m3Pin = 2;
@@ -28,87 +21,14 @@ static int m4Pin = 3;
 static int m5Pin = 4;
 static int m6Pin = 5;
 
-static float dt;
-static unsigned long prev_time;
-static unsigned long blink_counter, blink_delay;
-bool blinkAlternate;
-
-static unsigned long channel_1_pwm, channel_2_pwm, channel_3_pwm, channel_4_pwm, channel_5_pwm, channel_6_pwm;
-
-static float m1_command_scaled, m2_command_scaled, m3_command_scaled, m4_command_scaled, m5_command_scaled, m6_command_scaled;
 int m1_command_PWM, m2_command_PWM, m3_command_PWM, m4_command_PWM, m5_command_PWM, m6_command_PWM;
-static float s1_command_scaled, s2_command_scaled, s3_command_scaled, s4_command_scaled, s5_command_scaled, s6_command_scaled, s7_command_scaled;
-int s1_command_PWM, s2_command_PWM, s3_command_PWM, s4_command_PWM, s5_command_PWM, s6_command_PWM, s7_command_PWM;
-
-bool armedFly = false;
-
-static void scaleCommands() {
-
-  m2_command_PWM = m2_command_scaled*125 + 125;
-  m3_command_PWM = m3_command_scaled*125 + 125;
-  m4_command_PWM = m4_command_scaled*125 + 125;
-  m5_command_PWM = m5_command_scaled*125 + 125;
-  m6_command_PWM = m6_command_scaled*125 + 125;
-
-  m2_command_PWM = constrain(m2_command_PWM, 125, 250);
-  m3_command_PWM = constrain(m3_command_PWM, 125, 250);
-  m4_command_PWM = constrain(m4_command_PWM, 125, 250);
-  m5_command_PWM = constrain(m5_command_PWM, 125, 250);
-  m6_command_PWM = constrain(m6_command_PWM, 125, 250);
-
-  s1_command_PWM = s1_command_scaled*180;
-  s2_command_PWM = s2_command_scaled*180;
-  s3_command_PWM = s3_command_scaled*180;
-  s4_command_PWM = s4_command_scaled*180;
-  s5_command_PWM = s5_command_scaled*180;
-  s6_command_PWM = s6_command_scaled*180;
-  s7_command_PWM = s7_command_scaled*180;
-
-  s1_command_PWM = constrain(s1_command_PWM, 0, 180);
-  s2_command_PWM = constrain(s2_command_PWM, 0, 180);
-  s3_command_PWM = constrain(s3_command_PWM, 0, 180);
-  s4_command_PWM = constrain(s4_command_PWM, 0, 180);
-  s5_command_PWM = constrain(s5_command_PWM, 0, 180);
-  s6_command_PWM = constrain(s6_command_PWM, 0, 180);
-  s7_command_PWM = constrain(s7_command_PWM, 0, 180);
-
-}
-
-static void getCommands() {
-}
-
-static void failSafe() {
-
-  unsigned minVal = 800;
-  unsigned maxVal = 2200;
-  int check1 = 0;
-  int check2 = 0;
-  int check3 = 0;
-  int check4 = 0;
-  int check5 = 0;
-  int check6 = 0;
-
-  if (channel_1_pwm > maxVal || channel_1_pwm < minVal) check1 = 1;
-  if (channel_2_pwm > maxVal || channel_2_pwm < minVal) check2 = 1;
-  if (channel_3_pwm > maxVal || channel_3_pwm < minVal) check3 = 1;
-  if (channel_4_pwm > maxVal || channel_4_pwm < minVal) check4 = 1;
-  if (channel_5_pwm > maxVal || channel_5_pwm < minVal) check5 = 1;
-  if (channel_6_pwm > maxVal || channel_6_pwm < minVal) check6 = 1;
-
-  if ((check1 + check2 + check3 + check4 + check5 + check6) > 0) {
-    channel_1_pwm = channel_1_fs;
-    channel_2_pwm = channel_2_fs;
-    channel_3_pwm = channel_3_fs;
-    channel_4_pwm = channel_4_fs;
-    channel_5_pwm = channel_5_fs;
-    channel_6_pwm = channel_6_fs;
-  }
-}
 
 static void commandMotors() {
 
   int wentLow = 0;
+
   int pulseStart, timer;
+
   int flagM1 = 0;
   int flagM2 = 0;
   int flagM3 = 0;
@@ -122,6 +42,7 @@ static void commandMotors() {
   digitalWrite(m4Pin, HIGH);
   digitalWrite(m5Pin, HIGH);
   digitalWrite(m6Pin, HIGH);
+
   pulseStart = micros();
 
   while (wentLow < 6 ) { 
@@ -167,98 +88,6 @@ static void armMotors() {
   }
 }
 
-void calibrateESCs() {
-
-   while (true) {
-      prev_time = current_time;      
-      current_time = micros();      
-      dt = (current_time - prev_time)/1000000.0;
-
-      digitalWrite(13, HIGH); 
-
-      getCommands(); 
-      failSafe(); 
-
-      m1_command_scaled = thro_des;
-      m2_command_scaled = thro_des;
-      m3_command_scaled = thro_des;
-      m4_command_scaled = thro_des;
-      m5_command_scaled = thro_des;
-      m6_command_scaled = thro_des;
-      s1_command_scaled = thro_des;
-      s2_command_scaled = thro_des;
-      s3_command_scaled = thro_des;
-      s4_command_scaled = thro_des;
-      s5_command_scaled = thro_des;
-      s6_command_scaled = thro_des;
-      s7_command_scaled = thro_des;
-      scaleCommands(); 
-
-      commandMotors(); 
-
-      loopRate(2000); 
-   }
-}
-
-float floatFaderLinear(float param, float param_min, float param_max, float fadeTime, int state, int loopFreq){
-
-  float diffParam = (param_max - param_min)/(fadeTime*loopFreq); 
-
-  if (state == 1) { 
-    param = param + diffParam;
-  }
-  else if (state == 0) { 
-    param = param - diffParam;
-  }
-
-  param = constrain(param, param_min, param_max); 
-
-  return param;
-}
-
-float floatFaderLinear2(float param, float param_des, float param_lower, float param_upper, float fadeTime_up, float fadeTime_down, int loopFreq){
-
-  if (param > param_des) { 
-    float diffParam = (param_upper - param_des)/(fadeTime_down*loopFreq);
-    param = param - diffParam;
-  }
-  else if (param < param_des) { 
-    float diffParam = (param_des - param_lower)/(fadeTime_up*loopFreq);
-    param = param + diffParam;
-  }
-
-  param = constrain(param, param_lower, param_upper); 
-
-  return param;
-}
-
-static void loopBlink() {
-
-  if (current_time - blink_counter > blink_delay) {
-    blink_counter = micros();
-    digitalWrite(13, blinkAlternate); 
-
-    if (blinkAlternate == 1) {
-      blinkAlternate = 0;
-      blink_delay = 100000;
-      }
-    else if (blinkAlternate == 0) {
-      blinkAlternate = 1;
-      blink_delay = 2000000;
-      }
-  }
-}
-
-static void setupBlink(int numBlinks,int upTime, int downTime) {
-
-  for (int j = 1; j<= numBlinks; j++) {
-    digitalWrite(13, LOW);
-    delay(downTime);
-    digitalWrite(13, HIGH);
-    delay(upTime);
-  }
-}
-
 void setup() 
 {
   Serial.begin(500000); 
@@ -273,18 +102,7 @@ void setup()
   pinMode(m5Pin, OUTPUT);
   pinMode(m6Pin, OUTPUT);
 
-  digitalWrite(13, HIGH);
-
-  delay(5);
-
-  channel_1_pwm = channel_1_fs;
-  channel_2_pwm = channel_2_fs;
-  channel_3_pwm = channel_3_fs;
-  channel_4_pwm = channel_4_fs;
-  channel_5_pwm = channel_5_fs;
-  channel_6_pwm = channel_6_fs;
-
-  delay(10);
+  delay(15);
 
   m1_command_PWM = 125; 
   m2_command_PWM = 125;
@@ -292,20 +110,13 @@ void setup()
   m4_command_PWM = 125;
   m5_command_PWM = 125;
   m6_command_PWM = 125;
+
   armMotors(); 
-
-  setupBlink(3,160,70); 
-
 }
 
 void loop() 
 {
-
-  prev_time = current_time;      
   current_time = micros();      
-  dt = (current_time - prev_time)/1000000.0;
-
-  loopBlink(); 
 
   commandMotors(); 
 
