ebb442a77b5c6ec4514333daa7a6245b538b0dd4
simondlevy <simon.d.levy@gmail.com>
2023-12-22 20:42:49 -0500

    =Cleanup

--- examples/spin/spin.ino
+++ examples/spin/spin.ino
@@ -2,62 +2,53 @@
 
 #include <stdint.h>
 
-static unsigned long current_time;
+static const uint8_t m1Pin = 0;
 
-static void loopRate(int freq) {
+static uint8_t m1_command_PWM;
 
-  float invFreq = 1.0/freq*1000000.0;
-  unsigned long checker = micros();
-
-  while (invFreq > (checker - current_time)) {
-    checker = micros();
-  }
-}
-
-static int m1Pin = 0;
-
-int m1_command_PWM;
-
-static void commandMotors() {
-
+static void commandMotors() 
+{
   int wentLow = 0;
-
-  int pulseStart, timer;
-
   int flagM1 = 0;
 
   digitalWrite(m1Pin, HIGH);
 
-  pulseStart = micros();
+  auto pulseStart = micros();
 
   while (wentLow < 1) { 
 
-    timer = micros();
+    auto timer = micros();
 
     if ((m1_command_PWM <= timer - pulseStart) && (flagM1==0)) {
       digitalWrite(m1Pin, LOW);
       wentLow = wentLow + 1;
       flagM1 = 1;
     }
-
-    } 
   }
 }
 
-static void armMotors() {
-
+static void armMotors() 
+{
   for (int i = 0; i <= 50; i++) {
     commandMotors();
     delay(2);
   }
 }
 
+///////////////////////////////////////////////////////////
+
+static void loopDelay(const uint32_t freq, const uint32_t loopStartUsec) 
+{
+
+  float invFreq = 1.0/freq*1000000.0;
+  unsigned long checker = micros();
+
+  while (invFreq > (checker - loopStartUsec)) {
+    checker = micros();
+  }
+}
 void setup() 
 {
-  Serial.begin(500000); 
-
-  delay(500);
-
   pinMode(m1Pin, OUTPUT);
 
   delay(15);
@@ -69,11 +60,11 @@ void setup()
 
 void loop() 
 {
-  current_time = micros();      
+  const auto loopStartUsec = micros();      
 
   commandMotors(); 
 
-  loopRate(2000); 
+  loopDelay(2000, loopStartUsec); 
 
   static uint32_t prev;
   auto msec = millis();
